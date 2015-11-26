a5d2135474ddaef1f9e3c7abebfad5ac5abd3645
Ryan Goodrich <rygo6@me.com>
2015-11-26 01:15:19 -0800

    renamed sounds, added clickaudiosourceplay.cs

---
+++ Assets/Partak/Scripts/UI/ClickAudioSourcePlay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class ClickAudioSourcePlay : MonoBehaviour, IPointerClickHandler
+{
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		GetComponent<AudioSource>().Play();
+	}
+}
