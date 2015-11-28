86b63a63272f40e3dea9f5ad185f8912de71dc3f
Ryan Goodrich <rygo6@me.com>
2015-11-27 18:51:46 -0800

    adjusted all lightmaps

--- Assets/Partak/Editor/BatchBake.cs
+++ Assets/Partak/Editor/BatchBake.cs
@@ -7,7 +7,7 @@ public class BatchBake
 	[MenuItem("Assets/Bake")]
 	static public void Bake()
 	{
-		for (int i = 11; i < 19; ++i)
+		for (int i = 1; i < 19; ++i)
 		{
 			EditorApplication.OpenScene("Assets/Partak/Scenes/Level" + i + ".unity");
 			Lightmapping.Bake();
