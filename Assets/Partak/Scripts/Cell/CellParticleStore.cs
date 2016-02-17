1751f5f18194742929ea2a56dbe96f97b3b5a7da
Ryan Goodrich <rygo6@me.com>
2016-02-16 21:49:39 -0800

    reformatted some files, added menu and win music

--- Assets/Partak/Scripts/Cell/CellParticleStore.cs
+++ Assets/Partak/Scripts/Cell/CellParticleStore.cs
@@ -1,115 +1,89 @@
 ﻿using UnityEngine;
 using System;
+using System.Collections;
 
-namespace Partak
-{
-    public class CellParticleStore : MonoBehaviour
-	{
-		public CellParticle[] CellParticleArray { get; private set; }
+namespace Partak {
+public class CellParticleStore : MonoBehaviour {
 
-		public readonly int[] PlayerParticleCount = new int[MenuConfig.MaxPlayers];
+	public CellParticle[] CellParticleArray { get; private set; }
+	public readonly int[] PlayerParticleCount = new int[MenuConfig.MaxPlayers];
+	public readonly bool[] PlayerLost = new bool[MenuConfig.MaxPlayers];
+	CursorStore _cursorStore;
+	MenuConfig _playerSettings;
+	int _startParticleCount;
+	public event Action WinEvent;
+	public event Action<int> LoseEvent;
+	LevelConfig _levelConfig;
 
-		/// <summary> Keeps track of which players have lost. </summary>
-		public readonly bool[] PlayerLose = new bool[MenuConfig.MaxPlayers];
+	void Awake() {
+		_levelConfig = FindObjectOfType<LevelConfig>();
+		_cursorStore = FindObjectOfType<CursorStore>();
+		_playerSettings = Persistent.Get<MenuConfig>();
+		CellParticleArray = new CellParticle[_levelConfig.ParticleCount];
+		_startParticleCount = _levelConfig.ParticleCount / _playerSettings.ActivePlayerCount();
 
-		private CursorStore _cursorStore;
+		FindObjectOfType<CellParticleSpawn>().SpawnComplete += () => {
+			StartCoroutine(CalculatePercentages());
+		};
+	}
 
-		private MenuConfig _playerSettings;
+	public void IncrementPlayerParticleCount(int playerIndex) {
+		PlayerParticleCount[playerIndex]++;
+	}
 
-		private int _startParticleCount;
+	public void DecrementPlayerParticleCount(int playerIndex) {
+		PlayerParticleCount[playerIndex]--;
+	}
 
-//		private bool _recalculatePercentages;
-
-//		private bool _update;
-
-		public event Action WinEvent;
-
-		public event Action<int> LoseEvent;
-
-		private LevelConfig _levelConfig;
-
-		private void Awake()
-		{
-			_levelConfig = FindObjectOfType<LevelConfig>();
-			_cursorStore = FindObjectOfType<CursorStore>();
-			_playerSettings = Persistent.Get<MenuConfig>();
-			CellParticleArray = new CellParticle[_levelConfig.ParticleCount];
-			_startParticleCount = _levelConfig.ParticleCount / _playerSettings.ActivePlayerCount();
-
-			FindObjectOfType<CellParticleSpawn>().SpawnComplete += () =>
-			{
-				InvokeRepeating("CalculatePercentages", 1f, .1f);
-			};
-		}
-
-		public void IncrementPlayerParticleCount(int playerIndex)
-		{
-			PlayerParticleCount[playerIndex]++;
-		}
-
-		public void DecrementPlayerParticleCount(int playerIndex)
-		{
-			PlayerParticleCount[playerIndex]--;
-		}
-
-		private void CalculatePercentages()
-		{
-			for (int playerIndex = 0; playerIndex < PlayerParticleCount.Length; ++playerIndex)
-			{
+	IEnumerator CalculatePercentages() {
+		yield return new WaitForSeconds(2.0f);
+		while (true) {
+			for (int playerIndex = 0; playerIndex < PlayerParticleCount.Length; ++playerIndex) {
 				float percentage = (float)(PlayerParticleCount[playerIndex] - _startParticleCount) /
-				                   (float)(_levelConfig.ParticleCount - _startParticleCount);
+				                  (float)(_levelConfig.ParticleCount - _startParticleCount);
 				percentage = Mathf.Clamp(percentage, 0f, 1f) * 100f;	
 				_cursorStore.SetPlayerCursorMorph(playerIndex, percentage);
-
-				if (percentage == 100f)
-				{
-					CancelInvoke();
+				if (percentage == 100f) {
 					Win();
-				}
-				else if (PlayerParticleCount[playerIndex] == 0f && !PlayerLose[playerIndex])
-				{
-					PlayerLose[playerIndex] = true;
+					yield break;
+				} else if (PlayerParticleCount[playerIndex] == 0f && !PlayerLost[playerIndex]) {
+					PlayerLost[playerIndex] = true;
 					_cursorStore.PlayerLose(playerIndex);
 					LoseEvent(playerIndex);
 				}
 			}
-		}
-
-		//should be its own object WinSequence
-		public void Win()
-		{
-			_cursorStore.PlayerWin(WinningPlayer());
-			WinEvent();
-		}
-
-		public int WinningPlayer()
-		{
-			int count = 0;
-			int playerIndex = 0;
-			for (int i = 0; i < PlayerParticleCount.Length; ++i)
-			{
-				if (PlayerParticleCount[i] > count)
-				{
-					count = PlayerParticleCount[i];
-					playerIndex = i;
-				}
-			}
-			return playerIndex;
-		}
-
-		public int LosingPlayer()
-		{
-			int count = int.MaxValue;
-			int playerIndex = 0;
-			for (int i = 0; i < PlayerParticleCount.Length; ++i)
-			{
-				if (PlayerParticleCount[i] < count && !PlayerLose[i])
-				{
-					count = PlayerParticleCount[i];
-					playerIndex = i;
-				}
-			}
-			return playerIndex;
+			yield return new WaitForSeconds(.2f);
 		}
 	}
+
+	//should be its own object WinSequence
+	public void Win() {
+		_cursorStore.PlayerWin(WinningPlayer());
+		WinEvent();
+	}
+
+	public int WinningPlayer() {
+		int count = 0;
+		int playerIndex = 0;
+		for (int i = 0; i < PlayerParticleCount.Length; ++i) {
+			if (PlayerParticleCount[i] > count) {
+				count = PlayerParticleCount[i];
+				playerIndex = i;
+			}
+		}
+		return playerIndex;
+	}
+
+	public int LosingPlayer() {
+		int count = int.MaxValue;
+		int playerIndex = 0;
+		for (int i = 0; i < PlayerParticleCount.Length; ++i) {
+			if (PlayerParticleCount[i] < count && !PlayerLost[i]) {
+				count = PlayerParticleCount[i];
+				playerIndex = i;
+			}
+		}
+		return playerIndex;
+	}
+}
 }
--- Assets/Partak/Scripts/Component/AdvertisementDispatch.cs
+++ Assets/Partak/Scripts/Component/AdvertisementDispatch.cs
@@ -6,7 +6,7 @@ namespace Partak {
 public class AdvertisementDispatch : MonoBehaviour {
 
 	int _gameCount;
-	int _gameCountLimit = 4;
+	int _gameCountLimit = 2;
 	int _gameCountLimitAdd = 2;
 	int _sessionCount = 10;
 
