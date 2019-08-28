95e039c3532b128e414e39ae22c775521a6f0b0b
rygo6 <rygo6@me.com>
2019-08-27 22:35:32 -0700

    play menu working

--- Assets/GeoTetra/GTUI/Scripts/UIRenderer.cs
+++ Assets/GeoTetra/GTUI/Scripts/UIRenderer.cs
@@ -7,81 +7,127 @@ using UnityEngine;
 using GeoTetra.GTTween;
 using UnityEngine.UI;
 
-public class UIRenderer : MonoBehaviour
+namespace GeoTetra.GTUI
 {
-    [SerializeField] private float _displayUIOnStartDelay = .1f;
-    
-    [SerializeField] private UI _displayUIOnStart;
-
-    [SerializeField] private Camera _uiCamera;
-
-    [SerializeField] private CurveAsset _transitionCurve;
-
-    private Stack<UI> _priorUIs = new Stack<UI>();
-    private UI _currentUI;
-
-    private IEnumerator Start()
+    public class UIRenderer : MonoBehaviour
     {
-        yield return new WaitForSeconds(_displayUIOnStartDelay);
-        if (_displayUIOnStart != null)
-        {
-            InstantiateAndDisplayUI(_displayUIOnStart);
-        }
-    }
+        [SerializeField] private float _displayUIOnStartDelay = .1f;
 
-    public void InstantiateAndDisplayUI(UI ui)
-    {
-        UI uiInstance = Instantiate(ui);
-        DisplayUI(uiInstance);
-    }
-    
-    public void DisplayUI(UI ui)
-    {
-        ui.CurrentlyRenderedBy = this;
-        ui.Canvas.worldCamera = _uiCamera;
-        ui.Canvas.renderMode = RenderMode.ScreenSpaceCamera;
-        TweenIn(ui, Direction4.Up);
+        [SerializeField] private BaseUI _displayUIOnStart;
+
+        [SerializeField] private Camera _uiCamera;
+
+        [SerializeField] private CurveAsset _transitionCurve;
+
+        [SerializeField] private float _transitionMultiplier = 2;
         
-        if (_currentUI != null)
+        private Stack<BaseUI> _priorUIs = new Stack<BaseUI>();
+        private BaseUI _currentUI;
+        private BaseUI _currentModal;
+
+        private IEnumerator Start()
         {
-            UI priorUI = _currentUI;
-            TweenOut(priorUI, Direction4.Up,() => { priorUI.gameObject.SetActive(false); });
-            _priorUIs.Push(priorUI);
+            yield return new WaitForSeconds(_displayUIOnStartDelay);
+            if (_displayUIOnStart != null)
+            {
+                InstantiateAndDisplayUI(_displayUIOnStart);
+            }
         }
 
-        _currentUI = ui;
-    }
+        public void InstantiateAndDisplayUI(BaseUI ui)
+        {
+            BaseUI uiInstance = Instantiate(ui);
+            DisplayUI(uiInstance);
+        }
 
-    private void TweenIn(UI ui, Direction4 direction)
-    {
-        ui.gameObject.SetActive(true);
-        ui.Group.blocksRaycasts = false;
-        Vector2 newPos = ui.Root.anchoredPosition;
-        if (direction == Direction4.Up)
-            newPos.y = -ui.Root.rect.height;
-        else if (direction == Direction4.Down)
-            newPos.y = ui.Root.rect.height;
-        ui.Root.anchoredPosition = newPos;
-        StartCoroutine(RectTransformTweens.ToAnchoredPosition(ui.Root, Vector2.zero, 1, _transitionCurve.Curve,  () => ui.Group.blocksRaycasts = true));
-    }
-    
-    private void TweenOut(UI ui, Direction4 direction, Action OnFinish)
-    {
-        ui.Group.blocksRaycasts = false;
-        Vector2 newPos = ui.Root.anchoredPosition;
-        if (direction == Direction4.Up)
-            newPos.y = ui.Root.rect.height;
-        else if (direction == Direction4.Down)
-            newPos.y = -ui.Root.rect.height;
-        StartCoroutine(RectTransformTweens.ToAnchoredPosition(_currentUI.Root, newPos, 1, _transitionCurve.Curve, OnFinish));
-    }
-    
-    public void GoBack()
-    {
-        UI priorUI = _currentUI;
-        TweenOut(priorUI, Direction4.Down, () => { Destroy(priorUI.gameObject); });
+        public void DisplayModalUI(BaseUI ui)
+        {
+            ui.CurrentlyRenderedBy = this;
+            ui.Canvas.worldCamera = _uiCamera;
+            ui.Canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            ui.Group.blocksRaycasts = false;
+            ui.gameObject.SetActive(true);
+            _currentUI.Group.blocksRaycasts = false;
+            _currentModal = ui;
+            StartCoroutine(
+                Tweens.ToFloat(0,
+                    1,
+                    _transitionMultiplier,
+                    _transitionCurve.Curve,
+                    f => ui.Group.alpha = f,
+                    () =>
+                    {
+                        ui.Group.blocksRaycasts = true;
+                    }));
+        }
 
-        _currentUI = _priorUIs.Pop();
-        TweenIn(_currentUI, Direction4.Down);
+        public void CloseModal()
+        {
+            _currentModal.Group.blocksRaycasts = false;
+            StartCoroutine(
+                Tweens.ToFloat(
+                    1,
+                    0,
+                    _transitionMultiplier, 
+                    _transitionCurve.Curve,
+                    f => _currentModal.Group.alpha = f,
+                    () =>
+                    {
+                        Destroy(_currentModal.gameObject);
+                        _currentUI.Group.blocksRaycasts = true;
+                    }));
+        }
+
+        public void DisplayUI(BaseUI ui)
+        {
+            ui.CurrentlyRenderedBy = this;
+            ui.Canvas.worldCamera = _uiCamera;
+            ui.Canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            TweenIn(ui, Direction4.Up);
+
+            if (_currentUI != null)
+            {
+                BaseUI priorUI = _currentUI;
+                TweenOut(priorUI, Direction4.Up, () => { priorUI.gameObject.SetActive(false); });
+                _priorUIs.Push(priorUI);
+            }
+
+            _currentUI = ui;
+        }
+
+        private void TweenIn(BaseUI ui, Direction4 direction)
+        {
+            ui.gameObject.SetActive(true);
+            ui.Group.blocksRaycasts = false;
+            Vector2 newPos = ui.Root.anchoredPosition;
+            if (direction == Direction4.Up)
+                newPos.y = -ui.Root.rect.height;
+            else if (direction == Direction4.Down)
+                newPos.y = ui.Root.rect.height;
+            ui.Root.anchoredPosition = newPos;
+            StartCoroutine(RectTransformTweens.ToAnchoredPosition(ui.Root, Vector2.zero, _transitionMultiplier, _transitionCurve.Curve,
+                () => ui.Group.blocksRaycasts = true));
+        }
+
+        private void TweenOut(BaseUI ui, Direction4 direction, Action OnFinish)
+        {
+            ui.Group.blocksRaycasts = false;
+            Vector2 newPos = ui.Root.anchoredPosition;
+            if (direction == Direction4.Up)
+                newPos.y = ui.Root.rect.height;
+            else if (direction == Direction4.Down)
+                newPos.y = -ui.Root.rect.height;
+            StartCoroutine(RectTransformTweens.ToAnchoredPosition(_currentUI.Root, newPos, _transitionMultiplier, _transitionCurve.Curve,
+                OnFinish));
+        }
+
+        public void GoBack()
+        {
+            BaseUI priorUI = _currentUI;
+            TweenOut(priorUI, Direction4.Down, () => { Destroy(priorUI.gameObject); });
+
+            _currentUI = _priorUIs.Pop();
+            TweenIn(_currentUI, Direction4.Down);
+        }
     }
 }
