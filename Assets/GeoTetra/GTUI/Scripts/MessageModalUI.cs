2746bd0a3cd8c9703d82dbf34abb8cf73bd64e73
rygo6 <rygo6@me.com>
2019-09-03 00:22:34 -0700

    menus refactored, moved over to gamestate and scriptable objects

---
+++ Assets/GeoTetra/GTUI/Scripts/MessageModalUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+namespace GeoTetra.GTUI
+{
+	public class MessageModalUI : ModalUI
+	{
+		[SerializeField]
+		private Text _messageText;
+
+		private Action _action;
+
+		protected override void Close()
+		{
+			_action?.Invoke();
+			base.Close();
+		}
+
+		public void Init(string message, Action action = null)
+		{
+			_messageText.text = message;
+			_action = action;
+		}
+	}
+}
