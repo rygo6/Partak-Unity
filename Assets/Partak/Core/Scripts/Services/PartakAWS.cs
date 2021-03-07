7a0d45f9735d65b36644316d7e5f1f59feaead59
rygo6-MS <rygoodri@microsoft.com>
2021-03-07 02:07:44 -0800

    Key encoding working.

--- Assets/Partak/Core/Scripts/Services/PartakAWS.cs
+++ Assets/Partak/Core/Scripts/Services/PartakAWS.cs
@@ -1,22 +1,26 @@
 ﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon;
-using Amazon.CognitoIdentity;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.Model;
-using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Transfer;
 using Amazon.Util;
+using GeoTetra.GTCommon.Extensions;
 using GeoTetra.GTPooling;
 using GeoTetra.Partak;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
 
 namespace GeoTetra.GTBackend
 {
@@ -30,7 +34,9 @@ namespace GeoTetra.GTBackend
     [CreateAssetMenu(menuName = "GeoTetra/Services/PartakAWS")]
     public class PartakAWS : ServiceObject
     {
-        [SerializeField] private string _identityPoolId = "us-west-2:1c228a7e-eb85-433f-a708-d46b063a488f";
+        // [SerializeField] private string _accessKeyId = "";
+        // [SerializeField] private string _secretAccessKey = "";
+
         [SerializeField] private string _regionEndpoint  = "us-west-2";
         [SerializeField] private string _tableName = "Partak";
         [SerializeField] private string _s3Bucket = "partak";
@@ -58,6 +64,8 @@ namespace GeoTetra.GTBackend
         private AmazonS3Client _s3Client;
         private AmazonDynamoDBClient _dbClient;
         private TransferUtility _transferUtility;
+        private string _accessKeyId = "";
+        private string _secretAccessKey = "";
 
         protected override async Task OnServiceStart()
         {
@@ -80,6 +88,7 @@ namespace GeoTetra.GTBackend
         [ContextMenu("Connect")]
         private async Task Connect()
         {
+            Debug.Log("Connecting to AWS");
             if (_dbClient == null || _table == null || _s3Client == null || _transferUtility == null)
             {
                 LoggingConfig loggingConfig = AWSConfigs.LoggingConfig;
@@ -92,14 +101,21 @@ namespace GeoTetra.GTBackend
                 try
                 {
                     RegionEndpoint endpoint = RegionEndpoint.GetBySystemName(_regionEndpoint);
-                    CognitoAWSCredentials credentials = new CognitoAWSCredentials(_identityPoolId, endpoint);
-                    ImmutableCredentials credentialsResult = await credentials.GetCredentialsAsync();
-                    Debug.Log($"Credential Success: {credentialsResult.Token}");
-                    _dbClient = new AmazonDynamoDBClient(credentials, endpoint);
+                    
+                    UnityWebRequest request = UnityWebRequest.Get("https://qyqo9ud4hi.execute-api.us-west-2.amazonaws.com/PartakKey");
+                    request.SetRequestHeader("Authorization", "PartakKey218931987391793791392793");
+                    await request.SendWebRequest();
+                    string encrypted = request.downloadHandler.text;
+                    string decrypted = Encryption.DecryptStringAES(encrypted);
+                    string[] decryptedSplit = decrypted.Replace("\"", "").Split('-');
+                    _accessKeyId = decryptedSplit[0];
+                    _secretAccessKey = decryptedSplit[1];
+                    
+                    _dbClient = new AmazonDynamoDBClient(_accessKeyId, _secretAccessKey, endpoint);
                     Debug.Log($"AmazonDynamoDBClient Success");
                     _table = Table.LoadTable(_dbClient, _tableName);
                     Debug.Log($"{_tableName} Table Success");
-                    _s3Client = new AmazonS3Client(credentials, endpoint);
+                    _s3Client = new AmazonS3Client(_accessKeyId, _secretAccessKey, endpoint);
                     _transferUtility = new TransferUtility(_s3Client);
                     Debug.Log($"{_transferUtility} Success {GetInstanceID()}");
                 }
@@ -112,7 +128,7 @@ namespace GeoTetra.GTBackend
 
         public async Task DownloadLevel(string levelId)
         {
-            await Connect();
+            // await Connect();
             string levelPath = LevelUtility.LevelPath(levelId);
             string imagePath = LevelUtility.LevelImagePath(levelId);
 
@@ -136,13 +152,13 @@ namespace GeoTetra.GTBackend
         
         public async Task<Search> QueryLevelsCreatedAt(int pageSize)
         {
-            await Connect();
+            // await Connect();
             return QueryLevels(pageSize, _createdAtIndex);
         }
 
         public async Task<Search> QueryLevelsThumbsUp(int pageSize)
         {
-            await Connect();
+            // await Connect();
             return QueryLevels(pageSize, _thumbsUpIndex);
         }
         
@@ -165,7 +181,7 @@ namespace GeoTetra.GTBackend
         
         public async Task DownloadLevelPreview(string id, Texture2D image)
         {
-            await Connect();
+            // await Connect();
             string key = S3LevelImageKey(id);
             byte[] bytes = await GetImageBytesFromS3(key);
             image.LoadImage(bytes, true);
@@ -221,7 +237,7 @@ namespace GeoTetra.GTBackend
 
         public async Task SaveLevel(string levelId)
         {
-            await Connect();
+            // await Connect();
             
             Debug.Log("Saving level " + levelId);
             string levelPath = LevelUtility.LevelPath(levelId);
@@ -266,13 +282,13 @@ namespace GeoTetra.GTBackend
 
         public async Task IncrementThumbsUp(string levelID, bool positive)
         {
-            await Connect();
+            // await Connect();
             AtomicIncrement(levelID, LevelFields.ThumbsUpKey, positive);
         }
         
         public async Task IncrementThumbsDown(string levelID, bool positive)
         {
-            await Connect();
+            // await Connect();
             AtomicIncrement(levelID, LevelFields.ThumbsDownKey, positive);
         }
 
@@ -314,5 +330,188 @@ namespace GeoTetra.GTBackend
         {
             return $"{_levelImagesFolder}/{id}.png";
         }
+        
+        [ContextMenu("GenerateIvKey")]
+        private void GenerateIvKey()
+        {
+            try
+            {
+                using (RijndaelManaged rijndael = new RijndaelManaged())
+                {
+                    rijndael.Mode = CipherMode.CBC;
+                    rijndael.Padding = PaddingMode.PKCS7;
+                    rijndael.FeedbackSize = 128;
+                    rijndael.GenerateKey();
+                    rijndael.GenerateIV();
+                    Debug.Log(rijndael.Key.Length);
+                    Debug.Log(rijndael.IV.Length);
+
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.Append("{");
+                    Debug.Log("Key");
+                    for (int i = 0; i < rijndael.Key.Length; ++i)
+                    {
+                        stringBuilder.Append(rijndael.Key[i]);
+                        if (i != rijndael.Key.Length -1) stringBuilder.Append(",");
+                    }
+                    stringBuilder.Append("}");
+                    Debug.Log(stringBuilder.ToString());
+                    Debug.Log(new SoapHexBinary(rijndael.Key).ToString());
+
+                    stringBuilder.Clear();
+                    stringBuilder.Append("{");
+                    Debug.Log("IV");
+                    for (int i = 0; i < rijndael.IV.Length; ++i)
+                    {
+                        stringBuilder.Append(rijndael.IV[i]);
+                        if (i != rijndael.IV.Length -1) stringBuilder.Append(",");
+                    }
+                    stringBuilder.Append("}");
+                    Debug.Log(stringBuilder.ToString());
+                    Debug.Log(new SoapHexBinary(rijndael.IV).ToString());
+                    
+                    // string key = Encoding.UTF8.GetString(rijndael.Key);
+                    // string iv = Encoding.UTF8.GetString(rijndael.IV);
+                    //
+                    // Debug.Log($"key: {ToLiteral(key)}");
+                    // Debug.Log($"iv: {ToLiteral(iv)}");
+                    // byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+                    // byte[] ivBytes = Encoding.UTF8.GetBytes(iv);
+                    // Debug.Log(keyBytes.Length);
+                    // Debug.Log(ivBytes.Length);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"Error: {e.Message}");
+            }
+        }
+        
+        private static string ToLiteral(string input)
+        {
+            using (var writer = new StringWriter())
+            {
+                using (var provider = CodeDomProvider.CreateProvider("CSharp"))
+                {
+                    provider.GenerateCodeFromExpression(new CodePrimitiveExpression(input), writer, null);
+                    return writer.ToString();
+                }
+            }
+        }
+    }
+}
+
+
+public class Encryption {
+    public static string DecryptStringAES(string cipherText)
+    {
+        byte[] keybytes = {233,183,206,63,183,107,201,0,208,76,37,194,66,43,199,70,1,29,155,50,154,21,247,44,229,105,212,88,68,34,146,183};
+        byte[] iv = {47,16,90,33,110,27,57,223,77,48,98,236,250,218,217,179};
+        
+        // var keybytes = Encoding.UTF8.GetBytes("[�/V)����e�L`��\t�����>g�6�");
+        // var iv = Encoding.UTF8.GetBytes("{1�\"���{ܐek^�");
+
+        var encrypted = Convert.FromBase64String(cipherText);
+        var decriptedFromJavascript = DecryptStringFromBytes(encrypted, keybytes, iv);
+        return decriptedFromJavascript;
+    }
+    private static string DecryptStringFromBytes(byte[] cipherText, byte[] key, byte[] iv) {
+        // Check arguments.
+        if (cipherText == null || cipherText.Length <= 0) {
+            throw new ArgumentNullException("cipherText");
+        }
+        if (key == null || key.Length <= 0) {
+            throw new ArgumentNullException("key");
+        }
+        if (iv == null || iv.Length <= 0) {
+            throw new ArgumentNullException("key");
+        }
+
+        // Declare the string used to hold
+        // the decrypted text.
+        string plaintext = null;
+
+        // Create an RijndaelManaged object
+        // with the specified key and IV.
+        using(var rijAlg = new RijndaelManaged()) {
+            //Settings
+            rijAlg.Mode = CipherMode.CBC;
+            rijAlg.Padding = PaddingMode.PKCS7;
+            rijAlg.FeedbackSize = 128;
+
+            rijAlg.Key = key;
+            rijAlg.IV = iv;
+
+            // Create a decrytor to perform the stream transform.
+            var decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+            try {
+                // Create the streams used for decryption.
+                using(var msDecrypt = new MemoryStream(cipherText)) {
+                    using(var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read)) {
+
+                        using(var srDecrypt = new StreamReader(csDecrypt)) {
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+
+                        }
+
+                    }
+                }
+            } catch {
+                plaintext = "keyError";
+            }
+        }
+
+        return plaintext;
+    }
+
+    public static string EncryptStringAES(string plainText) {
+        var keybytes = Encoding.UTF8.GetBytes("[�/V)����e�L`��\t�����>g�6�");
+        var iv = Encoding.UTF8.GetBytes("{1�\"���{ܐek^�");
+
+        var encryoFromJavascript = EncryptStringToBytes(plainText, keybytes, iv);
+        return Convert.ToBase64String(encryoFromJavascript);
+    }
+
+    private static byte[] EncryptStringToBytes(string plainText, byte[] key, byte[] iv) {
+        // Check arguments.
+        if (plainText == null || plainText.Length <= 0) {
+            throw new ArgumentNullException("plainText");
+        }
+        if (key == null || key.Length <= 0) {
+            throw new ArgumentNullException("key");
+        }
+        if (iv == null || iv.Length <= 0) {
+            throw new ArgumentNullException("key");
+        }
+        byte[] encrypted;
+        // Create a RijndaelManaged object
+        // with the specified key and IV.
+        using(var rijAlg = new RijndaelManaged()) {
+            rijAlg.Mode = CipherMode.CBC;
+            rijAlg.Padding = PaddingMode.PKCS7;
+            rijAlg.FeedbackSize = 128;
+
+            rijAlg.Key = key;
+            rijAlg.IV = iv;
+
+            // Create a decrytor to perform the stream transform.
+            var encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+            // Create the streams used for encryption.
+            using(var msEncrypt = new MemoryStream()) {
+                using(var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write)) {
+                    using(var swEncrypt = new StreamWriter(csEncrypt)) {
+                        //Write all data to the stream.
+                        swEncrypt.Write(plainText);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+        }
+        // Return the encrypted bytes from the memory stream.
+        return encrypted;
     }
 }
